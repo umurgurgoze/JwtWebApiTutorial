9ee21debe03eacfa125ee7c5de4bd8e8fa76ea85
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-08-31 18:01:33 +0300

    Role based authorization added. Swagger authentication extention,authentication scheme and authentication middleware added.

--- JwtWebApiTutorial/Program.cs
+++ JwtWebApiTutorial/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +11,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( //9-Swagger header ekleyebilmek için ayarlarýmýz yapýyoruz.Swaggerda authorize butonu çýkýyor.
+    options =>
+    {
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Description = "Standart Authorization header using the Bearer scheme (\"bearer {token}\")",
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+
+        });
+        options.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
+
+
+//7-Authentication Scheme ekliyoruz.//////////////////////////////////////////
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+        .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateAudience = false,
+            ValidateIssuer = false
+        };
+    });
+/////////////////////////////////////////////////////////////////////////
 
 var app = builder.Build();
 
@@ -17,6 +51,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//////8-Authentication middleware ekledik. Authorization middleware'dan önce yazýlmalý.
+app.UseAuthentication();
+/////
 
 app.UseAuthorization();
 
